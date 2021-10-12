9621afca9f00d8ceee927caf96d7bdc569ce0d57
Reinier <rctromp@protonmail.com>
2021-10-12 13:04:18 +0200

    punten voor keuken, badkamer, wc en aftrekpunten toegevoegd

--- code/puntenstelsel.py
+++ code/puntenstelsel.py
@@ -39,8 +39,6 @@ kamernet = kamernet.drop_duplicates(subset=kamernet.columns.difference(['publica
 #TODO hier misschien een if-statement van maken: if in variable explore        
 del (adProperties, file, filename, reader, row, mergedRows)
 
-# # Filter kamers op html NOG DOEN MAAR DAN OP URL voor van voor 15-09!
-
 # # Filter kamers op url
 kamernet['woningtype'] = kamernet['kamers_url'].str.split('huren/').str[1]
 kamernet['woningtype'] = kamernet['woningtype'].str.split('-').str[0]
@@ -88,24 +86,36 @@ kamers_top.loc[kamers_top['punten_gemeenschappelijk'].isnull(), 'punten_totaal_n
 
 # Neem aan dat het CV heeft
 kamers_top['punten_cv'] = kamers_top['m2_kamer'] * 0.75
-kamers_top['punten_totaal'] = kamers_top['punten_totaal'] + kamers_top['punten_cv']
+
 
 # Kookgelegenheid
-# kamers_top['punten_keuken'] = kamers_top[(kamers_top['keuken'] == " Gedeeld ") + 5
 kamers_top['punten_keuken'] = 0
-kamers_top.loc[((kamers_top['keuken'] == " Gedeeld ") & (kamers_top['inwoners'] <= 5)), 'punten_keuken'] + 4
-kamers_top.loc[((kamers_top['keuken'] == " Gedeeld ") & (kamers_top['inwoners'] > 5)), 'punten_keuken'] + 0
-kamers_top.loc[((kamers_top['keuken'] == " Eigen ") & (kamers_top['m2_kamer'] <= 25)), 'punten_keuken'] + 10
-kamers_top.loc[((kamers_top['keuken'] == " Eigen ") & (kamers_top['m2_kamer'] > 25)), 'punten_keuken'] + 20
+kamers_top.loc[((kamers_top['keuken'] == " Gedeeld ") & (kamers_top['inwoners'] <= 5)), 'punten_keuken'] = kamers_top['punten_keuken'] + 4
+kamers_top.loc[((kamers_top['keuken'] == " Gedeeld ") & (kamers_top['inwoners'] > 5)), 'punten_keuken'] = kamers_top['punten_keuken'] + 0
+kamers_top.loc[((kamers_top['keuken'] == " Eigen ") & (kamers_top['m2_kamer'] <= 25)), 'punten_keuken'] = kamers_top['punten_keuken'] + 10
+kamers_top.loc[((kamers_top['keuken'] == " Eigen ") & (kamers_top['m2_kamer'] > 25)), 'punten_keuken'] = kamers_top['punten_keuken'] + 20
 
 
-#  WC
+#  WC 
+kamers_top['punten_toilet'] = 0
+kamers_top.loc[((kamers_top['toilet'] == " Gedeeld ") & (kamers_top['inwoners'] > 5)), 'punten_toilet'] = kamers_top['punten_keuken'] + 0
+kamers_top.loc[((kamers_top['toilet'] == " Gedeeld ") & (kamers_top['inwoners'] <= 5)), 'punten_toilet'] = kamers_top['punten_keuken'] + 2
+kamers_top.loc[(kamers_top['toilet'] == " Eigen "), 'punten_toilet'] = kamers_top['punten_keuken'] + 2
 
-# Douche & bad
+# Douche & bad & wastafel
+kamers_top['punten_badkamer'] = 0
+kamers_top.loc[((kamers_top['badkamer'] == " Gedeeld ") & (kamers_top['inwoners'] > 8)), 'punten_badkamer'] = kamers_top['punten_badkamer'] + 0
+kamers_top.loc[((kamers_top['badkamer'] == " Gedeeld ") & (kamers_top['inwoners'] <= 8)), 'punten_badkamer'] = kamers_top['punten_badkamer'] + 3
+kamers_top.loc[(kamers_top['badkamer'] == " Eigen "), 'punten_badkamer'] = kamers_top['punten_badkamer'] + 15 + 10
+kamers_top.loc[((kamers_top['badkamer'] == " Gedeeld ") & (kamers_top['inwoners'] <= 5)), 'punten_badkamer'] = kamers_top['punten_badkamer'] + 2
 
-# Wastafel 
+# Aftrekpunten
+kamers_top['aftrekpunten'] = 0
+kamers_top.loc[(kamers_top['m2_kamer'] < 10), 'aftrekpunten'] = kamers_top['aftrekpunten'] - 15 
+# WC via andere kamer kan ik niet zien
 
 # Bereken prijs
+kamers_top['punten_totaal'] = kamers_top['punten_totaal'] + kamers_top['punten_cv'] + kamers_top['punten_keuken'] + kamers_top['punten_toilet'] + kamers_top['punten_badkamer'] + kamers_top['aftrekpunten']
 punt_prijs = 2.21
 kamers_top['wettelijke_prijs'] = (kamers_top['punten_totaal'] * punt_prijs).round(0)
 # Als prijs hoger dan kamerprijs, True anders False
@@ -115,7 +125,7 @@ kamers_top.loc[kamers_top['prijs'] < kamers_top['wettelijke_prijs'], 'te_duur'] 
 # Drop alle kamers waar de url en wettelijke prijs hetzelfde zijn
 kamers_top = kamers_top.drop_duplicates(['kamers_url', 'wettelijke_prijs'])
 kamers_top['te_duur'].value_counts(normalize=True)
-
+ 
 # Deze regel checkt of er rijen zijn waar de kamers_url, oppervlakte_kamer, oppervlakte_subtitel en prijs gelijk zijn
 # als dat zo is is er een andere reden waarom de wettelijke prijs afwijkt, met de huidige dataset zijn dit 0 rijen
 unexplainedDuplicates = kamers_top[
@@ -126,10 +136,10 @@ unexplainedDuplicates = kamers_top[
 # Alle white spaces weg in het begin
 # Kijk per stad
 # Als er niet aan criteria voldaan wordt, bijvoorbeeld woningdetails niet ingevuld:
-# benaderen of een veilige aanname doen. 
-#
-#
-#
-#
-#
+# benaderen of een veilige aanname doen. (ik denk een gemiddelde bij 'onbekend')
+# Hoeveel is de punt_prijs? Ik snap het niet zo goed in de rekentool.
+# 
+# aannames
+# Geen Rijksmomunment (+50)
+# Geen wc via een andere kamer -15
 # =============================================================================
